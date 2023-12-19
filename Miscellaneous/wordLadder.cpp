b91d2bca670c10378fb5160303714a5f426aeb3e
Chirag Garg <95637160+Cgarg9@users.noreply.github.com>
2023-12-19 18:10:36 +0530

    Create wordLadder.cpp

---
+++ Miscellaneous/wordLadder.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/word-ladder/description/
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        st.erase(beginWord);
+        while (!q.empty()) {
+            string word = q.front().first;
+            int length = q.front().second;
+            q.pop();
+            if (word == endWord) {
+                return length;
+            }
+            for (int i = 0; i < word.length(); i++) {
+                char original = word[i];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    word[i] = ch;
+                    if (st.find(word) != st.end()) {
+                        q.push({word, length + 1});
+                        st.erase(word);
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        return 0;
+    }
+};
