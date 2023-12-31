7ea5eccffaa5a730230367b99a9924c2db6222ff
Shreekrishnaupadhyaya <1211shree@gmail.com>
2023-12-31 09:08:32 +0530

    create firstOccurence.cpp

--- searching/firstOccurence.cpp
+++ searching/firstOccurence.cpp
@@ -12,9 +12,6 @@ int firstOccurence(std::vector<int> &v, int e) {
        }
        v[m] > e ? r = m - 1 : l = m + 1;
    }
-
-   // element does not exists
-   if (i == -1) return -1;
    
    // searching the left side linearly to find the first occurence
    while (i > 0) {
@@ -23,14 +20,37 @@ int firstOccurence(std::vector<int> &v, int e) {
            break;
        }
    }
+
+   // returns -1 if element does not exists
    return i;
+}
+
+int firstOccurence_b(std::vector<int> &v, int e) {
+    // returns -1 if element is not found
+    int l = 0, r = v.size() - 1, i = -1;
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (v[m] == e) {
+            i = m;
+            // searching the left for first occurence using binary search
+            r = m - 1;
+        } else {
+            v[m] > e ? r = m - 1 : l = m + 1;
+        }
+    }
+    return i;
 }
 
 int main() {
     std::vector<int> v = {-1, 1, 1, 2, 3, 5, 6, 7, 8, 8, 9};
-    int element = 1;
+    int element = 9;
     int index = firstOccurence(v, element);
+    int idx = firstOccurence_b(v, element);
+    std::cout << "firstOccurence function's output" << std::endl;
     if (index == -1) std::cout << "Element doesn't exists" << std::endl;
     else std::cout << "First occurence of element is found at index " << index << std::endl;
+    std::cout << "firstOccurence_b function's output" << std::endl;
+    if (idx == -1) std::cout << "Element doesn't exists" << std::endl;
+    else std::cout << "First occrence of element is found at index " << idx << std::endl;
     return 0;
 }
